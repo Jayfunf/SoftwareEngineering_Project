6b21d519b07ac7e1a89e728f248f8511769bddaf
Jayfunf <simh3077@gmail.com>
2021-05-29 14:46:54 +0900

    modify : Button to ImageButton

--- app/src/main/java/com/example/softwareengineering_project/Home.java
+++ app/src/main/java/com/example/softwareengineering_project/Home.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
 public class Home extends AppCompatActivity {
-    Button BTN_back, BTN_mypage, BTN_search;
+    ImageButton BTN_mypage,BTN_back, BTN_search;
     EditText EditText_main;
     Array index;
     @Override
@@ -19,9 +20,9 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         EditText_main = (EditText)findViewById(R.id.EditText_main);
-        BTN_back = (Button)findViewById(R.id.back_BTN);
-        BTN_mypage = (Button)findViewById(R.id.mypage_BTN);
-        BTN_search = (Button)findViewById(R.id.search_BTN);
+        BTN_back = (ImageButton) findViewById(R.id.back_BTN);
+        BTN_mypage = (ImageButton) findViewById(R.id.mypage_BTN);
+        BTN_search = (ImageButton) findViewById(R.id.search_BTN);
 
         BTN_back.setOnClickListener(new View.OnClickListener() {
             @Override
