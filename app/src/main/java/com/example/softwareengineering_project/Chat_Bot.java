3f031c2d6531d8b008e23f1d7eada603e9c4c659
Jayfunf <simh3077@gmail.com>
2021-05-29 18:37:11 +0900

    modify : Chat_Bot.java

--- app/src/main/java/com/example/softwareengineering_project/Chat_Bot.java
+++ app/src/main/java/com/example/softwareengineering_project/Chat_Bot.java
@@ -3,12 +3,15 @@ package com.example.softwareengineering_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Chat_Bot extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
+        getSupportActionBar().hide();
+
+
     }
 }
