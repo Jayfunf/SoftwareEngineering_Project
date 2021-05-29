fc0eaa9e4e5882e928fae933c43770be2d491680
Jayfunf <simh3077@gmail.com>
2021-05-29 20:07:39 +0900

    modify : search_Activity.java

--- app/src/main/java/com/example/softwareengineering_project/search_Activity.java
+++ app/src/main/java/com/example/softwareengineering_project/search_Activity.java
@@ -2,19 +2,22 @@ package com.example.softwareengineering_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
 public class search_Activity extends AppCompatActivity {
     EditText search_Text;
+    ImageButton search_Enter_Button;
     Array index;
-
+    Intent BTN_intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +25,15 @@ public class search_Activity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         search_Text = (EditText)findViewById(R.id.editText_search);
-        //search_Text.setSelection(search_Text.length()); //커서 위치 설정이라는데 동작안함.
+        search_Enter_Button = (ImageButton)findViewById(R.id.search_enter_BTN);
 
+        //search_Text.setSelection(search_Text.length()); //커서 위치 설정이라는데 동작안함.
+        search_Enter_Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         search_Text.setOnKeyListener(new View.OnKeyListener(){
             @Override
             public boolean onKey(View v, int KeyCode, KeyEvent event) {
