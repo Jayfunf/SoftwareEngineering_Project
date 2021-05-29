7caeeecf602a1e23efdddd6a802b1640acd03e45
Jayfunf <simh3077@gmail.com>
2021-05-29 16:05:53 +0900

    modify : home_Button add : new Intent(home_Activity) for Home_button

--- app/src/main/java/com/example/softwareengineering_project/Home.java
+++ app/src/main/java/com/example/softwareengineering_project/Home.java
@@ -2,7 +2,9 @@ package com.example.softwareengineering_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.voice.VoiceInteractionSession;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,22 +14,29 @@ import android.widget.Toast;
 import java.lang.reflect.Array;
 
 public class Home extends AppCompatActivity {
-    ImageButton BTN_mypage,BTN_back, BTN_search;
+    ImageButton BTN_mypage,BTN_back, BTN_search, BTN_chatbot;
     EditText EditText_main;
     Array index;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        EditText_main = (EditText)findViewById(R.id.EditText_main);
+        Intent home_intent = new Intent(this, Home.class);
+        //EditText_main = (EditText)findViewById(R.id.EditText_main);
         BTN_back = (ImageButton) findViewById(R.id.back_BTN);
         BTN_mypage = (ImageButton) findViewById(R.id.mypage_BTN);
         BTN_search = (ImageButton) findViewById(R.id.search_BTN);
+        BTN_chatbot = (ImageButton) findViewById(R.id.chatbot_BTN);
+
 
         BTN_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Test_back_BTN",Toast.LENGTH_SHORT).show();
+                finish(); //terminate ex_Activity
+                startActivity(home_intent);
             }
         });
 
@@ -44,6 +53,11 @@ public class Home extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Test_search_BTN",Toast.LENGTH_SHORT).show();
             }
         });
-
+        BTN_chatbot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Test_chatbot_BTN",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
