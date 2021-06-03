e2cde5675832fba68d1e01909040a2199b7abd82
Jayfunf <simh3077@gmail.com>
2021-06-03 21:23:59 +0900

    modify : Home.java -----------add for Permission------------ add : checkPermissions() add : onRequestPermissionsResult() add : showNoPermissionToastAndFinish()

--- app/src/main/java/com/example/softwareengineering_project/Home.java
+++ app/src/main/java/com/example/softwareengineering_project/Home.java
@@ -2,12 +2,16 @@ package com.example.softwareengineering_project;
 //보관함, 번역, 알람, 근처약, 이미지
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,9 +24,15 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
+
+    public static final int MULTIPLE_PERMISSIONS = 1801;
+    private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     ImageButton BTN_mypage, BTN_back, BTN_search, BTN_chatbot, BTN_alarm, BTN_cabinet, BTN_translator,
     BTN_setting, BTN_store, BTN_image_search;
 
@@ -32,9 +42,12 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        checkPermissions();
+
         Intent home_intent = new Intent(this, Home.class);
         Intent search_intent = new Intent(this, search_Activity.class);
         Intent chat_bot_intent = new Intent(this, Chat_Bot.class);
+        Intent cv_intent = new Intent(this, cv_Activity.class);
 
         BTN_back = (ImageButton) findViewById(R.id.back_BTN);
         BTN_mypage = (ImageButton) findViewById(R.id.mypage_BTN);
@@ -113,7 +126,7 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Test_image_BTN", Toast.LENGTH_SHORT).show();
-                startActivity(home_intent);
+                startActivity(cv_intent);
                 finish(); //terminate ex_Activity
             }
         });
@@ -125,5 +138,48 @@ public class Home extends AppCompatActivity {
                 finish(); //terminate ex_Activity
             }
         });
+    }
+    private boolean checkPermissions() {
+        int result;
+        List<String> permissionList = new ArrayList<>();
+        for (String pm : permissions) {
+            result = ContextCompat.checkSelfPermission(this, pm);
+
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(pm);
+            }
+        }
+
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionList.toArray(new String[permissionList.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                boolean isDeny = false;
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            //permission denyed
+                            isDeny = true;
+                        }
+                    }
+                }
+                if (isDeny) {
+                    showNoPermissionToastAndFinish();
+                }
+            }
+        }
+    }
+    private void showNoPermissionToastAndFinish() {
+        Toast toast = Toast.makeText(this, "권한 요청에 동의 해주셔야 이용 가능합니다. 설정에서 권한 허용 하시기 바랍니다.", Toast.LENGTH_SHORT);
+        toast.show();
+        finish();
     }
 }
