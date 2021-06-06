39809c4ca976592b091b16c1aba9a0e51672f3cc
Jayfunf <simh3077@gmail.com>
2021-06-06 14:03:19 +0900

    modify : CV_Controller.java

--- app/src/main/java/com/example/softwareengineering_project/CV_Controller.java
+++ app/src/main/java/com/example/softwareengineering_project/CV_Controller.java
@@ -1,49 +1,39 @@
 package com.example.softwareengineering_project;
 
-import android.graphics.BitmapFactory;
+//카메라에 필요한 라이브러리?
+import android.content.Context;
 import android.hardware.Camera;
-
+//Tesseract << 사진 -> 문자열로 바꿔주는 API
 import com.googlecode.tesseract.android.TessBaseAPI;
 
+//opencv에 필요한 라이브러리
+import org.opencv.android.Utils;
 import org.opencv.core.*;
-import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import android.content.res.AssetManager;
+import java.util.*;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.hardware.Camera;
-import android.os.AsyncTask;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-//Tesseract << 사진 -> 문자열로 바꿔주는 API
-//opencv에 필요한 라이브러리
 
 public class CV_Controller {
-    TessBaseAPI tessBaseAPI = new TessBaseAPI();
 
+    public Bitmap CV_start(Mat inputmat){
 
-    public void CV_start() {
-        //inputmat은 캡쳐한 사진을 받으면 되는데 만약 캡쳐한 사진이 파일이라면 (사진이라면)
-        //takepicture.jpg로 저장했다고가정했습니다
-        Mat inputmat = Imgcodecs.imread("takepicture.jpg");
+        //Utils.bitmapToMat(); << bit to mat
+        //Utils.matToBitmap(); << mat to bit
+
+        //Mat inputmat = Imgcodecs.imread("takepicture.jpg");
         Mat cont = Contour(inputmat);
         // 이 cleaner 이라는 파일형식이 opencv_contour_img.jpg 파일입니다.
-        File cleaner = cleanimg(cont); // 이 파일을 Tesseract 에 넣어주기만 하면 됩니다.
+        Bitmap cleaner = cleanimg(cont); // 이 파일을 Tesseract 에 넣어주기만 하면 됩니다.
+
+        return cleaner;
+
     }
 
-    private static File cleanimg(Mat input) {
+    private static Bitmap cleanimg(Mat input) {
         //Mat inputmat = Imgcodecs.imread("sample_input1.jpg");
         Mat blur = new Mat();
         Mat output = new Mat();
@@ -63,12 +53,12 @@ public class CV_Controller {
 
         //제가 Tesseratc 가 입력받는 형식을 몰라서 일단 파일 만들어서 파일형식으로 리턴해줬어요.
         Imgcodecs.imwrite("opencv_contour_img.jpg", output);
-        File resultFile = new File("opencv_contour_img.jpg");
+        Bitmap resultFile = BitmapFactory.decodeFile("opencv_contour_img.jpg");
 
         return resultFile;
     }
 
-    public static Mat Contour(Mat imageMat) {
+    public static Mat Contour(Mat imageMat){
         Mat rgb = new Mat();
         rgb = imageMat.clone();
         Mat grayImage = new Mat();
@@ -86,7 +76,7 @@ public class CV_Controller {
         removeVerticalLines(gradThresh, 100);
         Imgproc.findContours(gradThresh, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
 
-        if (contours.size() > 0) {
+        if(contours.size() > 0) {
             for (int idx = 0; idx < contours.size(); idx++) {
                 Rect rect = Imgproc.boundingRect(contours.get(idx));
 
@@ -104,26 +94,26 @@ public class CV_Controller {
     }
 
     public static void removeVerticalLines(Mat img, int limit) {
-        Mat lines = new Mat();
+        Mat lines=new Mat();
         int threshold = 100;
         int minLength = 80;
         int lineGap = 5;
         int rho = 1;
 
-        Imgproc.HoughLinesP(img, lines, rho, Math.PI / 180, threshold, minLength, lineGap);
+        Imgproc.HoughLinesP(img, lines, rho, Math.PI/180, threshold, minLength, lineGap);
 
         for (int i = 0; i < lines.total(); i++) {
 
-            double[] vec = lines.get(i, 0);
+            double[] vec=lines.get(i,0);
             Point pt1, pt2;
 
-            pt1 = new Point(vec[0], vec[1]);
-            pt2 = new Point(vec[2], vec[3]);
+            pt1=new Point(vec[0],vec[1]);
+            pt2=new Point(vec[2],vec[3]);
 
-            double gapY = Math.abs(vec[3] - vec[1]);
-            double gapX = Math.abs(vec[2] - vec[0]);
+            double gapY = Math.abs(vec[3]-vec[1]);
+            double gapX = Math.abs(vec[2]-vec[0]);
 
-            if (gapY > limit && limit > 0) {
+            if(gapY>limit && limit>0) {
                 Imgproc.line(img, pt1, pt2, new Scalar(0, 0, 0), 10);
             }
         }
